716fb8a40c2776485e91d334a82ca1ba4ccaabdb
tkuczynski <tomkuc2@st.amu.edu.pl>
2025-03-18 21:18:54 +0100

    update: clean code

--- stats_script.sh
+++ stats_script.sh
@@ -2,11 +2,6 @@
 
 INPUT_FILE="final_data.csv"
 
-if [ ! -f "$INPUT_FILE" ]; then
-    echo "Error: File $INPUT_FILE not found!" > stats_result.txt
-    exit 1
-fi
-
 LINES=$(tail -n +2 "$INPUT_FILE" | wc -l)
 
 echo "Liczba rekordów w zbiorze: $LINES" > stats_result.txt
